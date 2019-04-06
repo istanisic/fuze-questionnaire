560f6a17061ea9b42126f13c7ee45e8e4b7a91a1
istanisic <ivanstanisic1@gmail.com>
2019-04-06 17:49:30 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React please
+            Learn React
           </a>
         </header>
       </div>
