c02c30602ef52ec5ac13528d7eb90e9aaa4563f1
istanisic <ivanstanisic1@gmail.com>
2019-04-15 18:37:06 -0400

    Added state to NewQuestion

--- src/components/NewQuestion/NewQuestion.js
+++ src/components/NewQuestion/NewQuestion.js
@@ -3,22 +3,31 @@ import './NewQuestion.css';
 // import DeleteButton from '../DeleteButton/DeleteButton';
 
 class NewQuestion extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      label: '',
+      options: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    e.target.id === "label" ? this.setState({label: e.target.value}) : this.setState({options: e.target.value});
+  }
 
   handleClick = (e) => {
     // e.preventDefault();
-    let label = document.getElementById("label").value;
-    let options = document.getElementById("options").value;
-    this.props.onClick(label, options.replace(/\s*,\s*/g, ",").split(','));
-    document.getElementById("label").value = "";
-    document.getElementById("options").value = "";
+    this.props.onClick(this.state.label, this.state.options.replace(/\s*,\s*/g, ",").split(','));
+    this.setState({label: '', options: []});
   }
 
   render() {
     return (
       <div className="NewQuestion">
         <p><strong>Add a M/C question:</strong></p>
-        <textarea rows="2" id="label" type="text" placeholder="What is your favourite programming language?" />
-        <textarea rows="4" id="options" type="text" placeholder="Enter your options separated by a comma. i.e Java, PHP, C, C++" />
+        <textarea rows="2" id="label" type="text" value={this.state.label} onChange={this.handleChange} placeholder="What is your favourite programming language?" />
+        <textarea rows="4" id="options" type="text" value={this.state.options} onChange={this.handleChange} placeholder="Enter your options separated by a comma. i.e Java, PHP, C, C++" />
         <a href="#" class="btn btn-primary" onClick={this.handleClick}>
           <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
           <span><strong>Add</strong></span>
